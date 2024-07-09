71bdbc23951f6899ddc4363f733bb8895a6d58aa
andreaheartily <andreaheartily08@gmail.com>
2024-07-09 19:10:05 +0300

    test

---
+++ experiment/test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i, num;
+    float sum, ave, mark;
+    printf("How many students? ");
+    scanf("%d",&num);
+    sum=0; //make it a practice to initialize the first value as 0
+    for(i=1; i<=num; i++)
+    {
+        printf("Enter the mark for student #%d:",i);
+        scanf("%f",&mark);
+        sum=sum+mark;
+    }
+    ave=sum/num;
+    printf("The average is %.2f.\n",ave);
+
+    return 0;
+}
